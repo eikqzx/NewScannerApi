663e0ee9a821425cfd69c8c19806c3a59ac18963
Attapon Sroysuwan <55727960+eikqzx@users.noreply.github.com>
2024-04-29 16:02:40 +0700

    Q

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from waitress import serve
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from lib.TwainBackEnd import ClassTwainBackEnd
+import os
+import base64
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -25,6 +27,21 @@ def getList():
     list = ClassTwainBackEnd.scannerList()
     # print("list:",list)
     return jsonify({"scannerList":list})
+
+@app.route('/createFile',methods=['POST'])
+def createFileByPath():
+    path = request.json.get('path')
+    fileBase64 = request.json.get('fileBase64')
+    if os.path.exists(path):
+        return {"result":False,"msg":f"File already exists at: {path}"}
+    else:
+        # Write the decoded image data to the file
+        decodeStr = base64.b64decode(fileBase64)
+        with open(path, 'wb') as file:
+            file.write(decodeStr)
+        return {"result":True,"msg":f"File created at: {path}"}
+
+
 
 mode = "dev"
 host = "0.0.0.0"
